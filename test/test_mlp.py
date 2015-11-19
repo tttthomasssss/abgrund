fa1b1e341df1b778590b2b8d9a8fa533762d8784
tttthomasssss <th0mas.ko6er@gmail.com>
2015-11-19 07:11:05 +0000

    better toy dataset mlp test

--- test/test_mlp.py
+++ test/test_mlp.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from mlp.feedforward import MLP
 
+
 def test_mlp_toy_dataset():
 	N = 100 # Number of Samples per Class
 	D = 2 # Number of Features (dimensionality of data)
@@ -26,12 +27,13 @@ def test_mlp_toy_dataset():
 	plt.close()
 	####
 
+	# eta=1.0; layer_size=200; lambda=0.1; activation=relu; weight_initialisation=xavier ###
 	gd_params = {
-		'step_rate': 1e-0
+			'step_rate': 1.0
 	}
 
-	mlp = MLP(shape=[(2, 100), 100, (100, 3), 3], dropout_proba=None, activation_fn='relu', max_epochs=10000,
-			  validation_frequency=1000, lambda_=1e-1, mini_batch_size=-1, W_init='randn', optimiser='gd', **gd_params)
+	mlp = MLP(shape=[(2, 200), 200, (200, 3), 3], dropout_proba=None, activation_fn='relu', max_epochs=10000,
+								  validation_frequency=1000, lambda_=0.1, mini_batch_size=-1, W_init='xavier', optimiser='gd', **gd_params)
 
 	mlp.fit(X, y)
 
@@ -47,7 +49,7 @@ def test_mlp_toy_dataset():
 	x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 	y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 	xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-	                     np.arange(y_min, y_max, h))
+						 np.arange(y_min, y_max, h))
 	Z = np.dot(np.maximum(0, np.dot(np.c_[xx.ravel(), yy.ravel()], W1) + b1), W2) + b2
 	Z = np.argmax(Z, axis=1)
 	Z = Z.reshape(xx.shape)
@@ -58,6 +60,50 @@ def test_mlp_toy_dataset():
 	plt.ylim(yy.min(), yy.max())
 	fig.savefig('/Users/thomas/DevSandbox/InfiniteSandbox/tag-lab/abgrund/test/dump/mlp_decision_boundary.png')
 	####
+
+	''' Hyperparam Tuning
+	for sr in [1., 0.1, 0.01, 0.001]:
+		for hh in [30, 50, 100, 200, 500]:
+			for l in [0.001, 0.01, 0.1, 1.]:
+				for afn in ['relu', 'sigmoid', 'tanh']:
+					for wi in ['xavier', 'randn']:
+
+						print('### CONFIG: eta={}; layer_size={}; lambda={}; activation={}; weight_initialisation={} ###'.format(sr, hh, l, afn, wi))
+
+						gd_params = {
+							'step_rate': sr
+						}
+
+						mlp = MLP(shape=[(2, hh), hh, (hh, 3), 3], dropout_proba=None, activation_fn=afn, max_epochs=10000,
+								  validation_frequency=1000, lambda_=l, mini_batch_size=-1, W_init=wi, optimiser='gd', **gd_params)
+
+						mlp.fit(X, y)
+
+						views = mlp.best_weights()
+						W1 = views[0]
+						b1 = views[1]
+						W2 = views[2]
+						b2 = views[3]
+
+						#### PLOT DECISION BOUNDARY
+						# plot the resulting classifier
+						h = 0.02
+						x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+						y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+						xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+											 np.arange(y_min, y_max, h))
+						Z = np.dot(np.maximum(0, np.dot(np.c_[xx.ravel(), yy.ravel()], W1) + b1), W2) + b2
+						Z = np.argmax(Z, axis=1)
+						Z = Z.reshape(xx.shape)
+						fig = plt.figure()
+						plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
+						plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+						plt.xlim(xx.min(), xx.max())
+						plt.ylim(yy.min(), yy.max())
+						fig.savefig('/Users/thomas/DevSandbox/InfiniteSandbox/tag-lab/abgrund/test/dump/mlp_decision_boundary_eta-{}_hidden-{}_lambda-{}_activation-{}_init-{}.png'.format(sr, hh, l, afn, wi))
+						####
+						print('-----------------------------------------------------------------------------')
+	'''
 
 if (__name__ == '__main__'):
 	test_mlp_toy_dataset()
