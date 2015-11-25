c170c4b3598abece529f6bc2ab919bf04415b238
tttthomasssss <tttthomasssss@gmail.com>
2015-11-25 15:01:39 +0000

    added elu activation fn

--- base/activation.py
+++ base/activation.py
@@ -10,6 +10,8 @@ def get_activation_fn_for_string(activation):
 		return (relu, deriv_relu)
 	elif (activation == 'tanh'):
 		return (tanh, deriv_tanh)
+	elif (activation == 'elu'):
+		return (elu, deriv_elu)
 
 
 def get_prediction_fn_for_string(prediction):
@@ -27,14 +29,9 @@ def deriv_softmax(y_pred, y_true):
 	return y_pred - y_true
 
 
-def sigmoid(x):# TODO: use scipy.special.expit!!!
+def sigmoid(x):
 	return expit(x)
-	'''
-	g = np.zeros(x.shape) # Numerical Stability, see: http://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
-	g[x >= 0] = 1 / (1 + np.exp(-x[x >= 0]))
-	g[x < 0] = 1 / (1 + np.exp(x[x < 0]))
-	return g
-	'''
+
 
 def deriv_sigmoid(x):
 	sigm = sigmoid(x)
@@ -56,3 +53,12 @@ def relu(x):
 
 def deriv_relu(x):
 	return np.float64(x > 0)
+
+
+def elu(x, alpha=1.): # Eqn. 16 - http://arxiv.org/pdf/1511.07289v1.pdf
+	# alternatively but a little slower: (x >= 0) * x + (x < 0) * (alpha * (np.exp(x) - 1))
+	return np.maximum(0, x) + alpha * (np.exp(np.minimum(0, x)) - 1)
+
+
+def deriv_elu(x, alpha=1.): # Eqn. 17 - http://arxiv.org/pdf/1511.07289v1.pdf
+	return np.float64(x >= 0) + (x < 0) * (alpha * (np.exp(x) - 1) + alpha)
